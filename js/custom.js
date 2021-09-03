79f3368f8daf317a50dae1f5e17250424aafb30e
adharsumon <m.sumon7080@gmail.com>
2021-09-03 11:18:19 +0600

    re upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-    // setTimeout(function(){
-    //     $('#loader').addClass('loaded');
-    // }, 3000);
+    setTimeout(function(){
+        $('#load').addClass('loaded');
+    }, 3000);
 
-    
+
 });
